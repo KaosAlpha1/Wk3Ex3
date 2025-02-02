13384bbef85d6059d9958094de782cc023565671
Michael Scott <michaelscott.msh@gmail.com>
2025-02-02 16:19:07 -0600

    Add project files.

---
+++ Wk3Ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Prompt the user to enter a string
+        Console.Write("Enter a string: ");
+
+        // Read the input string from the user and convert it to lowercase to handle case insensitivity
+        string input = Console.ReadLine().ToLower();
+
+        // "aeiou" contains all vowels, and we check how many characters in 'input' are vowels
+        int vowelCount = input.Count(c => "aeiou".Contains(c));
+
+        // Display the total count of vowels in the string
+        Console.WriteLine($"The number of vowels in the string is: {vowelCount}");
+
+        Console.ReadLine();
+    }
+}
